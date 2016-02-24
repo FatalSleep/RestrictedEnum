711f569fc9b8f5292d9fbd1b1474b5307fcecfcf
FatalSleep <fatalsleep@gmail.com>
2016-02-24 06:26:14 -0700

    Overhauled RestrictedEnum<T>.
    - Fixed not using generic type in functions.
    - Added Operators for Value Comparison.
    - Added Get the Underlying Enum Type.
    - Added Get Names of Enum Members.
    - Added Get Values of Enum Members.
    - Added Get Dictionary<Name,Value> for Enum Members.
    - Added Parse Function.
    - Added IComparable interface.

--- RestrictedEnum.cs
+++ RestrictedEnum.cs
@@ -1,28 +1,75 @@
-public class RestrictedEnum<T> where T : struct {
-    private System.Enum value;
-    public T Value { get { return (T)(object)value; } }
+public class RestrictedEnum<T> where T : struct, IComparable {
+    public T Flag { get; private set; }
+
+    public T[] Values {
+        get {
+            return (T[])Enum.GetValues(typeof(T));
+        }
+    }
+
+    public string[] Names {
+        get {
+            return Enum.GetNames(typeof(T));
+        }
+    }
 
     public RestrictedEnum() {
-        if ( !typeof(T).IsEnum ) throw new NotSupportedException();
-        value = null;
+        if(!typeof(T).IsEnum)
+            throw new NotSupportedException();
     }
 
-    public RestrictedEnum( Enum flag ) {
-        if ( !typeof(T).IsEnum ) throw new NotSupportedException();
-        SetFlag( flag );
+    public RestrictedEnum(T flag) : this() {
+        SetFlag(flag);
     }
 
-    public bool SetFlag( Enum flag ) {
-        bool checkEnum = Enum.IsDefined( typeof( T ), flag );
-        if ( checkEnum ) value = flag;
-        return checkEnum;
+    public T? Parse(string name) {
+        T value;
+        return Enum.TryParse<T>(name, out value) ? (T?)value : null;
     }
 
-    public bool HasFlag( Enum flag ) {
-        return ( Enum.IsDefined( typeof( T ), flag ) ) ? Enum.Equals( value, flag ) : false;
+    public IDictionary<string, T> Members() {
+        Dictionary<string, T> members = new Dictionary<string, T>();
+
+        foreach(string name in Names)
+            members.Add(name, (T)Enum.Parse(typeof(T), name));
+
+        return members;
     }
 
-    public T Parse() {
-        return (T)(object)value;
+    public bool SetFlag(T flag) {
+        bool check = Enum.Equals(Flag, flag) && Enum.IsDefined(typeof(T), flag);
+        if(check)
+            Flag = flag;
+        return check;
     }
+
+    public bool HasFlag(T flag) =>
+        Enum.Equals(Flag, flag);
+
+    public int CompareTo(RestrictedEnum<T> obj) =>
+        Flag.CompareTo(obj.Flag);
+
+    public static bool operator ==(RestrictedEnum<T> obj1, T obj2) =>
+        obj1.HasFlag(obj2);
+
+    public static bool operator ==(RestrictedEnum<T> obj1, RestrictedEnum<T> obj2) =>
+        obj1.HasFlag(obj2.Flag);
+
+    public static bool operator !=(RestrictedEnum<T> obj1, T obj2) =>
+        !obj1.HasFlag(obj2);
+
+    public static bool operator !=(RestrictedEnum<T> obj1, RestrictedEnum<T> obj2) =>
+        !obj1.HasFlag(obj2.Flag);
+
+    public Type GetEnumType() =>
+        typeof(T);
+
+    public bool Equals(RestrictedEnum<T> obj) =>
+        this == obj;
+
+    public override bool Equals(object obj) =>
+        base.Equals(obj);
+
+    public override int GetHashCode() =>
+        base.GetHashCode();
 }
